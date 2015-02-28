bb593622b1e487c00b509a0a2bc6eb3a70fabd5b
Danny G Smith <dgs@riskiii.com>
2015-02-28 16:17:51 +0000

    added styleguide

---
+++ style-guide/index.php
@@ -0,0 +1,74 @@
+<?php include_once('functions.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+  <title>Style Guide Boilerplate</title>
+  <meta name="viewport" content="width=device-width">
+  <!-- Style Guide Boilerplate Styles -->
+  <link rel="stylesheet" href="css/sg-style.css">
+
+  <!-- Replace below stylesheet with your own stylesheet -->
+  <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+
+<div id="top" class="sg-header sg-container">
+  <h1 class="sg-logo">STYLE GUIDE <span>BOILERPLATE</span></h1>
+  <form id="js-sg-nav" action=""  method="post" class="sg-nav">
+    <select id="js-sg-section-switcher" class="sg-section-switcher" name="sg_section_switcher">
+        <option value="">Jump To Section:</option>
+        <optgroup label="Intro">
+          <option value="#sg-about">About</option>
+          <option value="#sg-colors">Colors</option>
+          <option value="#sg-fontStacks">Font-Stacks</option>
+        </optgroup>
+        <optgroup label="Base HTML">
+          <?php listMarkupAsOptions('base'); ?>
+        </optgroup>
+        <optgroup label="Patterns">
+          <?php listMarkupAsOptions('patterns'); ?>
+        </optgroup>
+    </select>
+    <input type="hidden" name="sg_uri" value="<?php echo $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]; ?>">
+    <button type="submit" class="sg-submit-btn">Go</button>
+  </form><!--/.sg-nav-->
+</div><!--/.sg-header-->
+
+<div class="sg-body sg-container">
+  <div class="sg-info">
+
+    <div class="sg-colors sg-section">
+      <h2 class="sg-h2"><a id="sg-colors" class="sg-anchor">Colors</a></h2>
+        <div class="sg-color sg-color--a"><span class="sg-color-swatch"><span class="sg-animated">#E5E1D9</span></span></div>
+        <div class="sg-color sg-color--b"><span class="sg-color-swatch"><span class="sg-animated">#B50801</span></span></div>
+        <div class="sg-color sg-color--c"><span class="sg-color-swatch"><span class="sg-animated">#701713</span></span></div>
+        <div class="sg-color sg-color--d"><span class="sg-color-swatch"><span class="sg-animated">#352516</span></span></div>
+        <div class="sg-color sg-color--e"><span class="sg-color-swatch"><span class="sg-animated">#000000</span></span></div>
+        <div class="sg-markup-controls"><a class="sg-btn--top" href="#top">Back to Top</a></div>
+    </div><!--/.sg-colors-->
+
+    <div class="sg-font-stacks sg-section">
+      <h2 class="sg-h2"><a id="sg-fontStacks" class="sg-anchor">Font Stacks</a></h2>
+      <p class="sg-font sg-font-primary">'after_shokregular', sans-serif;</p>
+      <p class="sg-font sg-font-secondary">'Times New Roman', serif;</p>
+      <div class="sg-markup-controls"><a class="sg-btn--top" href="#top">Back to Top</a></div>
+    </div><!--/.sg-font-stacks-->
+  </div><!--/.sg-info-->
+
+  <div class="sg-base-styles">
+    <h1 class="sg-h1">Base HTML</h1>
+    <?php showMarkup('base'); ?>
+  </div><!--/.sg-base-styles-->
+
+  <div class="sg-pattern-styles">
+    <h1 class="sg-h1">Patterns<small> - Design and markup patterns unique to your site.</small></h1>
+    <?php showMarkup('patterns'); ?>
+  </div><!--/.sg-pattern-styles-->
+  </div><!--/.sg-body-->
+
+  <script src="js/sg-plugins.js"></script>
+  <script src="js/sg-scripts.js"></script>
+</body>
+</html>
+
